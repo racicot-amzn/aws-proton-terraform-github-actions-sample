8dd6b59a6a319048588fda84cfaec8dbab243368
sbx_user1051 <sbx_user1051@169.254.92.225>
2022-04-26 21:36:48 +0000

    Update requested for Proton - service/scheduled-lambda-svc/service-instance/my-instance

--- vpc-env/scheduled-lambda-svc-my-instance/main.tf
+++ vpc-env/scheduled-lambda-svc-my-instance/main.tf
@@ -1,9 +1,5 @@
 /*
-This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
-
-To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:074207182078:service/scheduled-lambda-svc/service-instance/my-instance
-
-If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+This file is no longer managed by AWS Proton. The associated resource has been deleted in Proton.
 */
 
 resource "aws_cloudwatch_event_rule" "lambda_schedule" {
