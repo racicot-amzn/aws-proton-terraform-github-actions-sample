bbf8e9f5345e8e4bc083815804e08955b69ab508
sbx_user1051 <sbx_user1051@169.254.209.133>
2022-04-26 21:01:23 +0000

    Update requested for Proton - service/scheduled-lambda-svc/service-instance/my-instance

---
+++ vpc-env/scheduled-lambda-svc-my-instance/locals.tf
@@ -0,0 +1,14 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:074207182078:service/scheduled-lambda-svc/service-instance/my-instance
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+locals {
+  account_id                = data.aws_caller_identity.current.account_id
+  region                    = data.aws_region.current.id
+  partition                 = data.aws_partition.current.id
+  using_default_lambda_code = var.service_instance.inputs.code_bucket == null ? true : false
+}
